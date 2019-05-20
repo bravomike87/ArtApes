8dcbb09f1901e3ee2bcee0b12212c11f2f02ffb0
Bjoern Muellenmeister <b.muellenmeister@gmail.com>
2019-05-21 00:13:40 +0200

    create profile model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/new'
+  get 'profiles/edit'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

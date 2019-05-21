187f0d3cfee43c3f25bd59bc8cacd09abe9c8bc7
Peter Linek <peter.linek@gmail.com>
2019-05-21 13:53:14 +0200

    seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 require "faker"
 
-puts 'Cleaning database User...'
+puts 'Cleaning all databases ...'
+
+Booking.destroy_all
+Artwork.destroy_all
 Profile.destroy_all
 User.destroy_all
 
-puts 'Cleaning database Profile...'
 
+puts 'Creating 15 fake Users and associated Profiles, Artworks and Bookings ...'
 
-
-
-puts 'Creating 30 fake Users and associated Profiles ...'
-30.times do
+20.times do
   user = User.create!(email: Faker::Internet.email, password: 'topsecret', password_confirmation: 'topsecret')
   Profile.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, user: user)
+  art = Artwork.create!(title: Faker::Games::Pokemon.name, description: Faker::Quote.most_interesting_man_in_the_world, tagline: Faker::Games::Pokemon.move, image: "https://images.unsplash.com/photo-1552510373-7a6449943736?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1309&q=80", price: 50, width: 20, height: 10, user: user)
+  Booking.create!(status: "available", artwork: art, user: user)
 end
-puts 'Finished database Profile!'
+
+puts 'Finished!'
+

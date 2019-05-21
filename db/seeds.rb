708ad0539d584d5966daabe83baba84a9a5f26d2
Agathe Pinel <agp.com@mailbox.org>
2019-05-21 18:26:05 +0200

    new seed to have different pictures on artwork pieces

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,13 @@ User.destroy_all
 
 puts 'Creating 15 fake Users and associated Profiles, Artworks and Bookings ...'
 
+index = 0
 20.times do
+  index = 0 if index > 9
+  index += 1
   user = User.create!(email: Faker::Internet.email, password: 'topsecret', password_confirmation: 'topsecret')
   Profile.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, user: user)
-  art = Artwork.create!(kind: Faker::Quote.singular_siegler, title: Faker::Games::Pokemon.name, description: Faker::Quote.most_interesting_man_in_the_world, tagline: Faker::Games::Pokemon.move, image: "https://images.unsplash.com/photo-1552510373-7a6449943736?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1309&q=80", price: 50, width: 20, height: 10, user: user)
+  art = Artwork.create!(kind: Faker::Quote.singular_siegler, title: Faker::Games::Pokemon.name, description: Faker::Quote.most_interesting_man_in_the_world, tagline: Faker::Games::Pokemon.move, image: "http://lorempixel.com/400/400/abstract/#{index}", price: 50, width: 20, height: 10, user: user)
   Booking.create!(status: "available", artwork: art, user: user)
 end
 

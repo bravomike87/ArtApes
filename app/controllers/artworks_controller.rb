1783838caab4812e2da536cd9c26d08f770b9168
Bjoern Muellenmeister <b.muellenmeister@gmail.com>
2019-05-21 16:32:29 +0200

    add create artwork for a user

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -6,12 +6,21 @@ class ArtworksController < ApplicationController
   end
 
   def show
+    @artwork = Artwork.find(params[:id])
   end
 
   def new
+    @artwork = Artwork.new
   end
 
   def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.user = current_user
+    if @artwork.save
+      redirect_to artwork_path(@artwork)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +30,11 @@ class ArtworksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:title, :description, :image, :kind, :price, :width, :height, :tagline)
   end
 end

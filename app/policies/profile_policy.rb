0a0b707bed2a57091ba0eeb6325c7cf3647fd7bd
Bjoern Muellenmeister <b.muellenmeister@gmail.com>
2019-05-22 11:32:31 +0200

    added pundit functions for artwork and profile(no delete)

--- app/policies/profile_policy.rb
+++ app/policies/profile_policy.rb
@@ -1,4 +1,4 @@
-class ArtworkPolicy < ApplicationPolicy
+class ProfilePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.all
@@ -9,23 +9,11 @@ class ArtworkPolicy < ApplicationPolicy
     true
   end
 
-  def create?
-    true
-  end
-
-  def new?
-    true
-  end
-
   def edit?
     record.user == user
   end
 
   def update?
-    record.user == user
-  end
-
-  def destroy?
     record.user == user
   end
 end

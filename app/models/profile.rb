0a4b88403be448d797bb2e16742bca95a77d1028
Agathe Pinel <agp.com@mailbox.org>
2019-05-23 13:11:51 +0200

    seed done with adress geoloc

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,13 @@
 class Profile < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
 
   mount_uploader :avatar, PhotoUploader
 
-  geocoded_by :address
 
-  validates :first_name, presence: true, allow_nil: true
-  validates :last_name, presence: true, allow_nil: true
-  validates :address, presence: true, allow_nil: true
+  # validates :first_name, presence: true, allow_nil: true
+  # validates :last_name, presence: true, allow_nil: true
+  # validates :address, presence: true, allow_nil: true
 
-  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,17 @@ user = User.create!(email: 'admin@artapes.com', password: 'topsecret', password_
 profile = user.profile
 profile.first_name = 'first'
 profile.last_name = 'last'
+profile.address = "Raumerstrasse 11, Berlin"
 
-avatar_url = "https://image.shutterstock.com/image-vector/blank-avatar-photo-placeholder-flat-260nw-1151124605.jpg"
-profile.remote_avatar_url = avatar_url
+# avatar_url = "https://image.shutterstock.com/image-vector/blank-avatar-photo-placeholder-flat-260nw-1151124605.jpg"
+# profile.remote_avatar_url = avatar_url
 profile.save
+sleep(1.2) # needed for geocode API
 
 puts 'Creating 20 fake Users and associated Profiles, and 1 Artwork each ...'
 
 index = 0
-20.times do
+10.times do
   index = 0 if index > 9
   index += 1
   user = User.create!(email: Faker::Internet.email, password: 'topsecret', password_confirmation: 'topsecret')
@@ -29,8 +31,10 @@ index = 0
 
   profile.first_name = Faker::Name.first_name
   profile.last_name = Faker::Name.last_name
-  profile.remote_avatar_url = avatar_url
+  # profile.remote_avatar_url = avatar_url
+  profile.address = "Raumerstrasse #{index}, Berlin"
   profile.save
+  sleep(1.2) # needed for geocode API
 
   art = Artwork.create!(kind: Faker::Quote.singular_siegler, title: Faker::Games::Pokemon.name, description: Faker::Quote.most_interesting_man_in_the_world, tagline: Faker::Games::Pokemon.move, price: "#{index * 10}", width: 20, height: 10, user: user)
 
